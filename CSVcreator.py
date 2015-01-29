1f4699810f1197127eeaa8ab644ee1eebcca2b1c
Jiri Konecny <dragonlichcz@gmail.com>
2015-01-29 13:17:56 +0100

    First version of CSVcreator.py complete
    File mods.csv is created from files in test_files folder

--- CSVcreator.py
+++ CSVcreator.py
@@ -5,25 +5,35 @@ import csv
 import re
 import utils
 
+COMPUTE_FOLDER = './test_files/'
+CSV_NAME = 'mods.csv'
+
+def createCSVfile(data):
+    with open(CSV_NAME, 'w', newline='') as csvfile:
+        writter = csv.writer(csvfile, delimiter=';')
+
+        for row in data:
+            writter.writerow(row)
+
 if __name__ == "__main__":
 
-    pattern = re.compile(r'(.*?).\d', flags=re.I)
+    pattern = re.compile(r'(.*?).\d*[.]', flags=re.I)
 
-    files = os.listdir('.')
-    filesHash = {}
+    files = os.listdir(COMPUTE_FOLDER)
+    filesHash = []
 
-    print(files)
 
     for f in files:
         if f.startswith('\.'):
             print(f + ' skipped')
             continue
 
+        print('Creating csv file for file: ' +  f)
+
         result = pattern.match(f)
 
         if result:
-            fHash = utils.md5sum(f)
-            print(result.groups())
-            filesHash[result.group()] = (result.group(), f, fHash)
+            fHash = utils.md5sum(COMPUTE_FOLDER + f)
+            filesHash.append((result.group(1), f, fHash))
 
-    print(filesHash)
+    createCSVfile(filesHash)
